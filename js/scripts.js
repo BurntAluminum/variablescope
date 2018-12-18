3eb2e40250cdcd3cd2494f02514b30e033c24350
Max Osborne <BurntAluminum@gmail.com>
2018-12-18 13:54:56 -0800

    This commit adds two lines of text you can click, WOW!

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  $("div#click-one").click(function(event) {
+    var whatToSay = "THICC VARIABLES";
+    alert(whatToSay);
+  })
+
+  $("div#click-two").click(function(event) {
+    var whatToSay = "I SAID THICC VARIABLES"
+    alert (whatToSay);
+  })
+});
